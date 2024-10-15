ae04c9ec80b027e821c56d327d5ea51ca6c3c67f
Sanfelix <sanfelix@yandex.ru>
2024-10-15 20:35:57 +0900

    ДЗ_3 решено

--- SEM3_DC/HW3.py
+++ SEM3_DC/HW3.py
@@ -2,14 +2,14 @@
 from pymongo import MongoClient
 import json
 
-client = MongoClient('localhost', 27017)
-db = client['hw3_erohin']
-book_info = db['book_info']
+client = MongoClient("localhost:27017")
+db = client.HW3_Erohin
+book_info = db.books_info
 
-with open('books_info.json', 'r') as file:
-    books_json = json.load(file)
-
-for book in books_json:
-    book_info.insert_one(book)
 
 books_data = book_info.find()
+
+count = book_info.count_documents({})
+print(count)
+
+
--- SEM3_DC/s03_unit01.py
+++ SEM3_DC/s03_unit01.py
@@ -14,17 +14,4 @@ with open('crash-data.json', 'r') as file:
 
 data = data['features']
 
-# Функция разделения данных на более мелкие фрагменты
-def chunk_data(data, chunk_size):
-    for i in range(0, len(data), chunk_size):
-        yield data[i:i + chunk_size]
 
-# Разделение данных на фрагменты по 5000 записей в каждом
-chunk_size = 5000
-data_chunks = list(chunk_data(data, chunk_size))
-
-# Вставка фрагментов в коллекцию MongoDB
-for chunk in data_chunks:
-    collection.insert_many(chunk)
-
-print("Данные успешно вставлены.")
